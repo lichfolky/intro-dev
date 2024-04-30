0205f17d35d89a6bc8e069f0b7f148f233e6e025
Mattia Folcarelli <mattia.folcarelli@gmail.com>
2024-04-30 18:31:09 +0200

    add es

---
+++ esercizi/esercizi-array.py
@@ -0,0 +1,8 @@
+## Conta quante volte è presente il 6 in un array
+numeri1 = [33,5, 3, 6, 43, 6, 787, 44]
+
+## Conta i numeri pari di un array
+numeri2 = [33,5, 3, 6, 43, 6, 787, 44]
+
+## Conta le vocali in una stringa
+stringa = "ciao come va"
--- esercizi/indovina-il-numero.py
+++ esercizi/indovina-il-numero.py
@@ -1,10 +1,22 @@
 numero_segreto = 9
+
 print("-"*40)
 print(" "*10+"INDOVINA IL NUMERO")
 print("-"*40)
 
 print("Il numero segreto è un numero tra 0 e 90,")
-print("cerca di indovinarlo in meno tentativi possibili")
-print("approfittando dei suggerimenti\n")
+print("cerca di indovinarlo in meno tentativi")
+print("possibili approfittando dei suggerimenti")
+print("i suggerimenti sono: oceano, acqua, fuochino, fuoco")
+print("per uscire premi contemporaneamente i pulsanti CTRL e C \n")
 
-numero = input("inserisci il numero")
+numero = int(input("inserisci un numero: "))
+finito = False
+while not finito:
+    # controllo se il numero è stato trovato
+    # se sì metto finito a True
+    # altrimenti dico di quanto sta sbagliando (acqua, fuoco...)
+    numero = int(input("nuovo numero: "))
+
+print("Hai trovato il numero " + str(numero_segreto))
+
