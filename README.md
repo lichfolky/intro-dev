99cc1b5176a2e19c1da7a261fb72b9571d7aadda
Lichfolky <lichfolky@gmail.com>
2024-05-07 15:00:33 +0200

    update

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # Introduzione alla programmazione
 
-1 - 
+1 - [intro](intro.md)
+2 - [python](python.md)
+3 - [condizioni e cicli](condizioni-cicli.md)
+2 - [funzioni](funzioni.md)
---
+++ esercizi/palla8.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+import random
+
+risposte = [
+    "Per quanto posso vedere, sì",
+    "È certo",
+    "È decisamente così",
+    "Molto probabilmente",
+    "Le prospettive sono buone",
+    "I segni indicano di sì",
+    "Senza alcun dubbio",
+    "Sì",
+    "Sì, senza dubbio",
+    "Ci puoi contare",
+    "È difficile rispondere, prova di nuovo",
+    "Rifai la domanda più tardi",
+    "Meglio non risponderti adesso",
+    "Non posso predirlo ora",
+    "Concentrati e rifai la domanda",
+    "Non ci contare",
+    "La mia risposta è no",
+    "Le mie fonti dicono di no",
+    "Le prospettive non sono buone",
+    "Molto incerto",
+]
+
+numero = random.randint(0, 19)
+
+print(risposte[numero])
