8bb68dff7365e5b7c8d222353c3c896c8814f198
Lichfolky <lichfolky@gmail.com>
2024-05-08 18:24:08 +0200

    es

--- esercizi/esercizi-array2.py
+++ esercizi/esercizi-array2.py
@@ -1,1 +1,13 @@
-####
+# Dato l'array ["Hello", "World","in" ,"a","frame"] creare:
+
+# *********
+# * Hello *
+# * World *
+# * in    *
+# * a     *
+# * frame *
+# *********
+
+# Data una parola creare tutti gli anagrammi possibili
+
+# Data una parola valutare se è palindroma
