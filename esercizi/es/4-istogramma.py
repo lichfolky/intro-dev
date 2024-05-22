b75f2de19dd0414cf4b82a93e55268fc1b3d19ac
Mattia Folcarelli <mattia.folcarelli@gmail.com>
2024-05-22 18:15:40 +0200

    istogramma

--- esercizi/es/4-istogramma.py
+++ esercizi/es/4-istogramma.py
@@ -2,4 +2,20 @@ def istogramma(data):
     for dato in data:
         print("*" * dato)
 
+
 istogramma([3, 4, 5, 5, 2])
+
+""" 
+0: ***
+1: ****
+2: *****
+3: *****
+4: ** 
+"""
+
+def istogramma_numeri(data):
+    for i in range(len(data)):
+        print(str(i) + ": ", "*" * data[i], sep="")
+
+print()
+istogramma_numeri([3, 4, 5, 5, 2])
